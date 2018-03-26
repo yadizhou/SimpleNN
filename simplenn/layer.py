fix gradient

simplenn/layer.py
@@ -69,7 +69,7 @@ class Linear(Layer):
 
     def Backward(self, dataIn, dataOut, gradIn):
         self.dW = dataIn.T @ gradIn
-        self.db = gradIn.mean(axis=0)
+        self.db = gradIn.sum(axis=0)
         return gradIn @ self.W.T
 
     def Predict(self, dataOut):
@@ -256,6 +256,7 @@ LAYER = {
     "MAX_POOLING_2D": MaxPooling2d,
 
     "DROPOUT"       : Dropout,
+    # "BATCH_NORM"    : BatchNorm,
 
 }
 

simplenn/loss.py
@@ -36,7 +36,7 @@ class BCE(Loss):
         return -np.sum(y * np.log(h) + (1 - y) * np.log1p(-h)) / y.shape[0]
 
     def Backward(self, y, h):
-        return (h - y) / y.shape[0]
+        return (h - y) / (1 - h) / h / y.shape[0]
 
 
 class CE(Loss):  # Takes output from Linear layer, not Softmax layer